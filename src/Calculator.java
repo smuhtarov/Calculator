e77122cff94121f66375c49d33f51ed3e497f97a
smuhtarov <mauhtarov@gmail.com>
2019-01-19 12:53:55 -0500

    write add method

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,4 +3,16 @@ public class Calculator {
     public static double add(double num1, double num2) {
         return num1 + num2;
     }
+
+    public static double sub(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public static double mult(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public static double div(double num1, double num2) {
+        return num1 / num2;
+    }
 }
